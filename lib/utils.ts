059ea1d32d0f932b5ab84ad3b5e88408295a32ba
Taisija <ti00245@rvt.lv>
2025-04-23 21:14:38 +0300

    utils.ts fixing

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,10 @@
-import { clsx } from 'clsx';
+import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import type { ClassValue } from 'clsx';
-
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Combines multiple class names and merges Tailwind CSS classes efficiently
+ * @param inputs - Class names to combine
+ * @returns Merged class string
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
